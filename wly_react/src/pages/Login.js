8558c404839b8826e471114c449b088de40e8ed9
sagarworks1288 <sagarworks1288@gmail.com>
2023-08-23 12:12:15 +0530

    login added

---
+++ wly_react/src/pages/Login.js
@@ -0,0 +1,49 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import _ from "lodash"
+import axios from "axios"
+import { googleAuthId, baseUrl } from '../utils/config'
+
+
+export default function Login() {
+
+    const auth = (token) => {
+        let data = JSON.stringify({ token });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://admirable-creponne-011375.netlify.app/api/client/sign-up',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+                alert(JSON.stringify(response.data))
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+
+    }
+
+
+    return (<>
+        <GoogleOAuthProvider clientId={googleAuthId}>
+            <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                    console.log(credentialResponse);
+                    auth(_.get(credentialResponse, 'credential', null))
+                }}
+                onError={() => {
+                    console.log("Login Failed");
+                }}
+            />
+        </GoogleOAuthProvider>
+    </>)
+}
---
+++ wly_react/src/utils/config.js
@@ -0,0 +1,2 @@
+export const googleAuthId = '209904001034-q9235oqklob1ebgqkikk0oqav4b0la0j.apps.googleusercontent.com'
+export const baseUrl = 'https://admirable-creponne-011375.netlify.app'
