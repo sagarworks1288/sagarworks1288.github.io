e5b1c82185a321f9ac663cae7ccc4f5be460d01a
sagarworks1288 <sagarworks1288@gmail.com>
2023-08-26 15:35:32 +0530

    post enable

--- wly_react/src/pages/Login.js
+++ wly_react/src/pages/Login.js
@@ -1,49 +1,36 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GoogleLogin } from "@react-oauth/google";
-import _ from "lodash"
-import axios from "axios"
-import { googleAuthId, baseUrl } from '../utils/config'
-
+import _ from "lodash";
+import axios from "axios";
+import { googleAuthId, baseUrl } from "../utils/config";
 
 export default function Login() {
+  const auth = (token) => {
 
-    const auth = (token) => {
-        let data = JSON.stringify({ token });
+    const postData = { token };
 
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: 'https://funx1650.netlify.app/.netlify/functions/sign-up',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: data
-        };
+    axios.post("https://funx1650.netlify.app/.netlify/functions/sign-up", postData).then((response) => {
+        console.log(JSON.stringify(response.data));
+        alert(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-        axios.request(config)
-            .then((response) => {
-                console.log(JSON.stringify(response.data));
-                alert(JSON.stringify(response.data))
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-
-    }
-
-
-    return (<>
-        <GoogleOAuthProvider clientId={googleAuthId}>
-            <GoogleLogin
-                onSuccess={(credentialResponse) => {
-                    console.log(credentialResponse);
-                    auth(_.get(credentialResponse, 'credential', null))
-                }}
-                onError={() => {
-                    console.log("Login Failed");
-                }}
-            />
-        </GoogleOAuthProvider>
-    </>)
+  return (
+    <>
+      <GoogleOAuthProvider clientId={googleAuthId}>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+            auth(_.get(credentialResponse, "credential", null));
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </GoogleOAuthProvider>
+    </>
+  );
 }
