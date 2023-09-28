a9b03f35b5b26e68089118107c46fab773c0ba6e
sagarworks1288 <sagarworks1288@gmail.com>
2023-09-28 16:34:32 +0530

    replit server

--- wly_react/src/utils/config.js
+++ wly_react/src/utils/config.js
@@ -1,2 +1,2 @@
 export const googleAuthId = "209904001034-q9235oqklob1ebgqkikk0oqav4b0la0j.apps.googleusercontent.com";
-export const baseUrl = "http://localhost:5002";
+export const baseUrl = "https://wlyreplitexpress.sagarworks.repl.co";
