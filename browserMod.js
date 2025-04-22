d1f60e842ae4e1a5f7a5dc0f0447401fdf9985e9
sagarworks1288 <112943379+sagarworks1288@users.noreply.github.com>
2025-04-22 18:29:17 +0530

    Update browserMod.js

--- browserMod.js
+++ browserMod.js
@@ -1,1 +1,31 @@
-console.log("RTX ON");
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://viayoo.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @run-at       document-end
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+
+
+// Example logs
+  console.log("Hello, RTX!");
+ 
+   console.log("Hello, RTX!");
+
+
+////////////////////////////////
+
+
+
+
+
+})();
